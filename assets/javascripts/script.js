89d8295f999a91d754b899a503bd7dd962ab80f1
Arshad Chummun <arshad@chummun.com>
2013-01-23 16:25:47 +0400

    Add a primary class to .form-actions button

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,3 +1,8 @@
 (function ($) {
-  // code here
+  $(document).ready(function() {
+
+    // add a primary class to form-actions
+    $('.form-actions .btn').first().addClass('btn-primary');
+
+  });
 })(jQuery);
