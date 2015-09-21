8b59a7c8d07e5a2b90b3c704e3075a2afaffc598
Arshad Chummun <arshad@chummun.com>
2015-09-22 00:13:33 +0400

    Update display for menus and events

--- templates/event/views-view--content--events--upcoming-events.tpl.php
+++ templates/event/views-view--content--events--upcoming-events.tpl.php
@@ -8,7 +8,7 @@
 <?php foreach ($rows as $cols): ?>
   <div class="view__row row">
     <?php foreach ($cols as $col): ?>
-      <div class="view__col col-md-4 margin--md--bottom">
+      <div class="view__col col-md-4 col-sm-6 margin--md--bottom">
         <?php print $col; ?>
       </div>
     <?php endforeach; ?>
