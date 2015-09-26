2cd3d96d02101fe0f199bfc6891a79c3c3ec6b3b
Arshad Chummun <arshad@chummun.com>
2015-09-26 22:57:01 +0400

    Fix responsive for menu node

--- templates/menu/node--menu--full.tpl.php
+++ templates/menu/node--menu--full.tpl.php
@@ -6,13 +6,13 @@
 ?>
 <article class="<?php print $classes; ?>">
   <?php if (!empty($title)): ?>
-    <h1 class="menu__title clear-margin--top margin--md--bottom visible-xs visible-sm">
+    <h1 class="menu__title clear-margin--top margin--md--bottom visible-xs">
       <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
     </h1>
   <?php endif; ?>
 
   <div class="row">
-    <div class="col-md-5">
+    <div class="col-sm-5">
       <?php if (!empty($content['field_menu_images'])): ?>
         <div class="menu__image margin--sm--bottom">
           <?php if (!empty($content['featured'])): ?>
@@ -22,27 +22,27 @@
         </div>
       <?php endif; ?>
     </div>
-    <div class="col-md-7">
+    <div class="col-sm-7">
       <?php if (!empty($title)): ?>
-        <h1 class="menu__title clear-margin--top visible-md visible-lg">
+        <h1 class="menu__title clear-margin--top hidden-xs">
           <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
         </h1>
-      <?php endif; ?>
-
-      <?php if (!empty($content['field_menu_one_liner'])): ?>
-        <small class="menu__one-liner border--xs--bottom margin--sm--bottom padding--xs--bottom"><?php print render($content['field_menu_one_liner']); ?></small>
       <?php endif; ?>
 
       <?php if (!empty($content['field_menu_nutrition_types'])): ?>
         <div class="menu__nutrition-types"><?php print render($content['field_menu_nutrition_types']); ?></div>
       <?php endif; ?>
 
-      <?php if (!empty($content['field_menu_variants'])): ?>
-        <div class="menu__variants border--xs--top"><?php print render($content['field_menu_variants']); ?></div>
+      <?php if (!empty($content['body'])): ?>
+        <div class="menu__text margin--sm--top"><?php print render($content['body']); ?></div>
       <?php endif; ?>
 
-      <?php if (!empty($content['body'])): ?>
-        <div class="menu__text margin--md--top"><?php print render($content['body']); ?></div>
+      <?php if (!empty($content['field_menu_one_liner'])): ?>
+        <small class="menu__one-liner"><?php print render($content['field_menu_one_liner']); ?></small>
+      <?php endif; ?>
+
+      <?php if (!empty($content['field_menu_variants'])): ?>
+        <div class="menu__variants border--xs--top"><?php print render($content['field_menu_variants']); ?></div>
       <?php endif; ?>
     </div>
   </div>
